2cdeee2fb668d3e519b12efc2682887f1953e7b8
himanshu-ydv01 <himanshu44ydv@gmail.com>
2026-03-15 13:13:26 +0530

    Added okhttp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try(Response response = client.newCall(request).execute()){
+            if(!response.isSuccessful()){
+                System.out.println(response.code());
+            }
+
+            System.out.println(response.body().string());
+        }catch (IOException ex){
+            ex.printStackTrace();
         }
     }
 }
