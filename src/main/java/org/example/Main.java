409d39c40892aa806570407eb97b6eda2514089d
himanshu-ydv01 <himanshu44ydv@gmail.com>
2026-03-28 13:35:58 +0530

    Added Retrofit with Gson

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,29 +2,41 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        OkHttpClient client = new OkHttpClient();
+//        OkHttpClient client = new OkHttpClient();
+//
+//        String url = "https://jsonplaceholder.typicode.com/todos/1";
+//
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .build();
+//
+//        try(Response response = client.newCall(request).execute()){
+//            if(!response.isSuccessful()){
+//                System.out.println(response.code());
+//            }
+//
+//            System.out.println(response.body().string());
+//        }catch (IOException ex){
+//            ex.printStackTrace();
+//        }
 
-        String url = "https://jsonplaceholder.typicode.com/todos/1";
 
-        Request request = new Request.Builder()
-                .url(url)
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        try(Response response = client.newCall(request).execute()){
-            if(!response.isSuccessful()){
-                System.out.println(response.code());
-            }
-
-            System.out.println(response.body().string());
-        }catch (IOException ex){
-            ex.printStackTrace();
-        }
+        TodoService todoService = retrofit.create(TodoService.class);
+        Todo t = todoService.getTodoById("1").execute().body();
+        System.out.println("org.example.Todo String generated is " + t.toString());
     }
 }
